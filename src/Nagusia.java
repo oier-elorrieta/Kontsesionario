00b4fc7f64f8b0672b0877d84fadc3bf8a5c4339
OPE <kk@kkk.es>
2020-02-12 13:21:29 +0100

    Ibilgailuak sortuta

--- src/Nagusia.java
+++ src/Nagusia.java
@@ -3,6 +3,8 @@ public class Nagusia {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Ibilgailua ib1 = new Ibilgailua("ibil1");
+		Ibilgailua ib2 = new Ibilgailua("ibil2");
 
 	}
 
