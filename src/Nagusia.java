805d5b2c73234bd45726136fd1927750794f826d
OPE <kk@kk.es>
2020-02-12 13:36:10 +0100

    commit

--- src/Nagusia.java
+++ src/Nagusia.java
@@ -2,11 +2,7 @@
 public class Nagusia {
 
 	public static void main(String[] args) {
-		System.out.println("hola mundo");
-		System.out.println("F en chat de RIP");
-		System.out.println("Aitor para ya dhholmlñe cambiar");
-		System.out.println("daadasdasdassd");
-		System.out.println("goien callate ya!!!!");
+		System.out.println("adios mundo");
 	}
 
 }
