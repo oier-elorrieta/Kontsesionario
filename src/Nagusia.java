7a27de818536275109be843cfba1d4d259ebe41a
AitorU <aitoru@kk.com>
2020-02-12 12:56:07 +0100

    F en el chat

--- src/Nagusia.java
+++ src/Nagusia.java
@@ -4,7 +4,7 @@ public class Nagusia {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hola mundo");
-		System.out.println("F en los chat de RIP");
+		System.out.println("F en chat de RIP");
 	}
 
 }
